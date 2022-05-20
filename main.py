b9e10101d8d70d16cced967ca3a817c4e1248684
Rustam Shestov <rus.shestov2014@yandex.ru>
2022-05-21 04:53:34 +0500

    Подключил бутстрап

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>HELLO WORLD</h1> <a href='about'>Перейди</a>"
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return "<h1>ABOUT</h1>  <a href='/'>На главную</a>"
+    return render_template("about.html")
 
 
 @app.route("/cart")
